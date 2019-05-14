eef2360615198791ea2aa716735c5d1dd9b2458c
Thomas Kirouac <thomaskirouac99@gmail.com>
2019-05-14 09:28:40 -0400

    no message

--- DB_API/src/Main/MySqlCon.java
+++ DB_API/src/Main/MySqlCon.java
@@ -1,6 +1,7 @@
 package Main;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -16,6 +17,7 @@ class MySqlCon {
 	public void openLocalConnection(){
 	  try
 	  {
+
 		  Class.forName("com.mysql.jdbc.Driver");
 		  con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sys","root","");
 	  }
